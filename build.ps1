8f9ef485f482ffeb8e2305a4d06f62d57935381a
码魂 <pkptzx@126.com>
2025-12-23 23:39:52 +0800

    Disable net35 build in build.ps1
    Comment out net35 build commands in build.ps1.

--- build.ps1
+++ build.ps1
@@ -4,9 +4,9 @@ $ErrorActionPreference = 'Stop'
 # dotnet build isn't used because it can't build net35 tfms
 #
 
-msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net35 de4dot.netframework.sln
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-Remove-Item Release\net35\*.pdb, Release\net35\*.xml, Release\net35\Test.Rename.*
+# msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net35 de4dot.netframework.sln
+# if ($LASTEXITCODE) { exit $LASTEXITCODE }
+# Remove-Item Release\net35\*.pdb, Release\net35\*.xml, Release\net35\Test.Rename.*
 
 msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net45 de4dot.netframework.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
